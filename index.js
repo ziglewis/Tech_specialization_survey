634347d7da1ce235bceaf8a1e76752c326c9acfc
ziglewis <ziglewis48@gmail.com>
2023-07-15 20:27:58 +0100

    added reload on return

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ mySubmitBtn.addEventListener("submit", function(){
     myEntireForm.reset() 
     
 })
+
+window.onpopstate = function(event) {
+    // Check if the URL has a hash.
+    if (!window.location.hash) {
+      // If not, add a hash and reload the page.
+      window.location = window.location + '#loaded';
+      window.location.reload();
+    }
+  };
